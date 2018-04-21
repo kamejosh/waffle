54eaf8739b5721ca8c4edea2639de0ecf4a583bb
Josh <kamejosh@gmail.com>
2018-04-21 14:17:16 +0200

    cleanup Arduino test integration

--- src/WifiConnection/W_ArduinoWifi.cpp
+++ src/WifiConnection/W_ArduinoWifi.cpp
@@ -6,17 +6,14 @@
 //set project namespace
 using namespace waffle;
 
-//class specific defines
-static const unsigned int MAX_CONNECTION_ATTEMPTS = 10;
-
 ArduinoWiFi::ArduinoWiFi() : IWiFi()
 {
-    m_client = new WiFiClass();
+    m_client = new WiFiClient();
 }
 
 ArduinoWiFi::ArduinoWiFi(char* ssid, char* pwd) : IWiFi(ssid, pwd)
 {
-    m_client = new WiFiClass();
+    m_client = new WiFiClient();
 }
 
 ArduinoWiFi::~ArduinoWiFi()
@@ -26,24 +23,7 @@ ArduinoWiFi::~ArduinoWiFi()
 
 bool ArduinoWiFi::connect()
 {
-    Serial.print("Connecting to WiFi..");
-    WiFi.begin(m_ssid, m_pwd);
-
-    unsigned int connectionAttempts = 0;
-    while (WiFi.status() != WL_CONNECTED)
-    {
-      Serial.print(".");
-      ++connectionAttempts;
-
-      if(connectionAttempts >= MAX_CONNECTION_ATTEMPTS)
-      {
-        Serial.println("Maximum number of connection attempts exceeded. Abort.");
-        return false;
-      }
-    }
-
-    Serial.println("\nConnection to WiFi successful!");
-    return true;
+    
 }
 
 #endif
